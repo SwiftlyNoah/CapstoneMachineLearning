f4d2fcc99c9e01ad7305b731ea5952afb9bf50d4
Noah Brauner <nbbrauner@gmail.com>
2021-01-16 19:49:54 -0500

    Initial Commit

---
+++ MachineLearningCapstone/MachineLearningCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MachineLearningCapstoneApp.swift
+//  MachineLearningCapstone
+//
+//  Created by Noah Brauner on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct MachineLearningCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
